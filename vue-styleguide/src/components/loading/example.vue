6f858bfe0228ba207a074b3aaae2926844af081a
mafengyan <2937735461@qq.com>
2020-06-05 15:00:23 +0800

    增加example的组件运行环境

---
+++ vue-styleguide/src/components/loading/example.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="exam">
+    <p class="txt">示例：</p>
+    <p class="txt txt1">全局loading:</p>
+    <div @click="doClick" class="btn">点我试一试</div>
+    <p class="txt txt1">局部loading:</p>
+    <div @click="doClick1" class="btn">点我试一试</div>
+    <rLoading type="0" v-if="show"></rLoading>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import loading from './index.js'
+Vue.use(loading)
+export default {
+  data () {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    doClick () {
+      let loading1 = this.$loading({
+        propsData: {
+          msg: '3s后消失'
+        }
+      })
+      setTimeout(() => loading1.remove(), 3000)
+    },
+    doClick1 () {
+      this.show = true
+    }
+  }
+}
+</script>
+<style scoped>
+.exam .txt {
+  margin: 15px 0;
+  font-size: 16px;
+  font-weight: bold;
+  color: navy;
+}
+.exam .txt1 {
+  color: royalblue;
+}
+.exam .btn {
+  width: 90px;
+  padding: 10px;
+  border: 1px solid pink;
+  background: #fff;
+  font-size: 14px;
+  color: palevioletred;
+  text-align: center;
+}
+</style>
--- vue-styleguide/src/components/loading/readme.md
+++ vue-styleguide/src/components/loading/readme.md
@@ -1,18 +1,34 @@
-# 使用方法
-```jsx static
-import loading from "Loading_Component_Path"
+#### 使用方法
+
+```js static
+import loading from 'Loading_Component_Path'
 Vue.use(loading)
 ```
 
-```jsx static
-//页面loading
-let loading = this.$loading(); //显示
-loading.remove(); //删除
+loading, use default message '加载中'
 
+```js static
+let loading = this.$loading()
+loading.remove() //删除loading实例
 ```
-```jsx static
-//加载更多loading
+
+loading, use custome message
+
+```js static
+let loading1 = this.$loading({
+  propsData: {
+    msg: '支付时间较长，预计5s结束'
+  }
+})
+loading1.remove() //删除loading实例
+```
+
+页面内加载更多 loading
+
+```html
 <rLoading></rLoading>
 ```
-* 原型链上挂载的是页面loading浮层， this.$loading()
-* 下拉加载更多,页面塞全局组件rLoading
+
+```[import](./example.vue)
+Text typed here will be entirely ignored. You can use it to describe the example imported for maintenance purposes
+```
